041a47784d91450e5ce5b28ad05713040cb557e5
Yaseen <yasODST3@DESKTOP-5MRJ2LA>
2017-04-19 11:41:43 +0100

    Bookings can be filtered by date with the click of a button - Yaseen

--- PBFrontEnd/ListOfBookings.aspx.cs
+++ PBFrontEnd/ListOfBookings.aspx.cs
@@ -39,6 +39,49 @@ public partial class ListOfBookings : System.Web.UI.Page
         Int32 Index = 0;
         // clear the list of any existing items
         lstBookings.Items.Clear();
+        // get the count of records
+        RecordCount = Bookings.Count;
+        // loop through each record found using the index
+        while (Index < RecordCount)
+        {
+            // get the name of the destination
+            BookingPrice = Convert.ToDecimal(Bookings.BookingList[Index].TotalPrice);
+            // get the price per person for each destination
+            BookingApproved = Convert.ToString(Bookings.BookingList[Index].BookingApproved);
+            // get the ID of each destination
+            BookingID = Convert.ToString(Bookings.BookingList[Index].BookingID);
+            // get the booking date of the flight
+            BookingDate = Convert.ToString(Bookings.BookingList[Index].BookingDate);
+            // set up a new object of class list item
+            ListItem NewItem = new ListItem(BookingPrice + " " + BookingApproved + " " + BookingDate, BookingID);
+            // add the item to the list
+            lstBookings.Items.Add(NewItem);
+            // increment the index
+            Index++;
+        }
+        // return the number of records found
+        return RecordCount;
+    }
+
+    // function to populate list box with bookings to be processed
+    Int32 DisplayBookingsByDate()
+    {
+        // create an instance of the booking collection
+        clsBookingCollection Bookings = new clsBookingCollection();
+        // var for record count
+        Int32 RecordCount;
+        // var for total booking price
+        decimal BookingPrice;
+        // var for booking approved 
+        string BookingApproved;
+        // var for booking date
+        string BookingDate;
+        // var for Booking ID
+        string BookingID;
+        // var for Index
+        Int32 Index = 0;
+        // clear the list of any existing items
+        lstBookings.Items.Clear();
         // filter the records
         Bookings.FilterByDate();
         // get the count of records
@@ -93,7 +136,7 @@ public partial class ListOfBookings : System.Web.UI.Page
         //declare var to store the record count
         Int32 RecordCount;
         //assign the results of the display destinations function to the record count var
-        RecordCount = DisplayBookings();
+        RecordCount = DisplayBookingsByDate();
         //display the number of records found
         lblError.Text = RecordCount + " records found";
     }
